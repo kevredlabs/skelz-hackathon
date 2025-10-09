7bae2c4bd47d500d28633980def44b941d930409
Léo Marquand <leo-marquand@hotmail.fr>
2025-10-09 13:56:48 +0200

    program tested

--- contracts/programs/skelz/src/lib.rs
+++ contracts/programs/skelz/src/lib.rs
@@ -2,21 +2,38 @@
 // Temporary: Anchor macro emits a deprecated realloc; safe to ignore here
 use anchor_lang::prelude::*;
 
-declare_id!("11111111111111111111111111111111");
+declare_id!("4uw8DwTRdUMwGmbNrK5GZ5kgdVtco4aUaTGDnEUBrYKt");
 
 #[program]
 pub mod skelz {
     use super::*;
 
-    pub fn hello(_ctx: Context<Hello>) -> Result<()> {
-        msg!("Hello, Skelz!");
+    pub fn write_signature(ctx: Context<WriteSignature>, digest: String) -> Result<()> {
+        let signature = &mut ctx.accounts.signature;
+        signature.digest = digest;
+        signature.signer = ctx.accounts.signer.key();
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Hello<'info> {
+#[instruction(digest: String)]
+pub struct WriteSignature<'info> {
     // Keep one signer to be explicit and future-proof if needed
-    #[account(signer)]
+    #[account(mut,signer)]
     pub signer: Signer<'info>,
+    #[account(
+    init,
+    payer = signer,
+    space = 8 + 32 + 32,
+    seeds = [b"signature", digest.as_bytes()],
+    bump)]
+    pub signature: Account<'info, Signature>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Signature {
+    pub digest: String,
+    pub signer: Pubkey,
 }
